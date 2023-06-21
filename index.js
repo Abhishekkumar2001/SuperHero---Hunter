9d1c6db69dc6c432a9783726283bda8b9e8e3c45
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-06-21 10:42:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,132 +1,83 @@
-const url = 'https://superheroapi.com/api.php/2928355607286861';
-const searchBox = document.getElementById('search');
-const searchResultsContainer = document.getElementById('search-results-container');
+// My Public API - c2595c6e10b8e75e6bd3b3c61b14547c
+// MD5 Hash -  77964d9b5c2bef6213992685d7c2dfd4
 
-// load event listeners
-loadEventListeners();
-function loadEventListeners(){
-  searchBox.addEventListener('keyup', handleSearch);
-}
+//  Get Id from HTML File, & Search query with HTTP Request, then parse it
 
-// when a user clicks enter in the search bar
-async function handleEnter(name){
-  let data = await fetchAsync(`${url}/search/${name}`);
-  // redirect to super hero page if success
-  if(data.response === 'success'){
-    console.log(data);
-    let path = `${window.location.pathname} + /../superhero.html#id=${data.results[0].id}`;
-    window.open(path);  
-  }
-}
-
-// handle search 
-async function handleSearch(e){
-  // trim the query name
-  let name = e.target.value.trim();
-  // check if user has hit enter in the search bar
-  if(e.keyCode === 13 && name.length > 0){
-    handleEnter(name);
-  }
-  if(name.length == 0){
-    await clearResults();
-  }
-  else{
-    // fetch results
-    let data = await fetchAsync(`${url}/search/${name}`);
-    if(data && data.response === 'success'){
-      searchResultsContainer.innerHTML = "";
-      let favs = getFavs();
-      // create a list of elements for search results and add event listeners
-      for(let i = 0; i < data.results.length; i++){
-        let item = document.createElement('div');
-        item.className = "search-item";
-        item.setAttribute('id', `${data.results[i].id}`);
-
-        let label = document.createElement('div');
-        label.innerHTML = data.results[i].name;
-        label.addEventListener('click', viewHeroPage);
-        item.appendChild(label);
-
-        let option = document.createElement('div');
-        if(favs.includes(data.results[i].id)){
-          option.innerHTML = "Remove from favourites";
-          option.addEventListener('click', removeFromFavourites);  
-        }
-        else{
-          option.innerHTML = "Add to favourites";
-          option.addEventListener('click', addToFavourites);  
-        }
-        item.appendChild(option);
-
-        searchResultsContainer.appendChild(item);
-      }
+document.getElementById("search-form").addEventListener('keyup' , function(){
+    var url = getUrl();
+    var xhrRequest = new XMLHttpRequest();
+    xhrRequest.open('get',url,true);
+    xhrRequest.send();    
+    xhrRequest.onload = function(){
+        var data = JSON.parse(xhrRequest.responseText);
+        display(data);  
     }
-    else{
-      await clearResults();
+});
+
+
+// Get the URL from  API
+function getUrl(){
+    // From Id I'll get value.
+    var searchQuery = document.getElementById('search-string').value;
+    // Print the Search Query in to Console.
+    console.log(searchQuery);
+    //  Set the main heading for user to know what he/she searched for.
+    document.getElementById('querySection').innerHTML = 'You have searched for : '+ searchQuery;
+//  If search query matches the results then it will execute next function/command.
+    if(!searchQuery){
+        console.log('Name cannot be empty!');
+        return "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4"
+    }else{
+        return `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${searchQuery}&apikey=c2595c6e10b8e75e6bd3b3c61b14547c&hash=77964d9b5c2bef6213992685d7c2dfd4&ts=1`
     }
-  }
 }
 
-// fetch results from API
-async function fetchAsync (url) {
-  try{
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;  
-  }catch(err){
-    await clearResults();
-  }
-}
+//  Get Canvas 
+let canvas = document.getElementById('canvas');
+// Get Search String
+let searchHero = document.getElementById('search-string').value;
 
-// clear search results
-async function clearResults(){
-  let i = searchResultsContainer.childNodes.length;
-  while(i--){
-    searchResultsContainer.removeChild(searchResultsContainer.lastChild);
-  }
-}
 
-// redirect to a super hero page with respective id
-async function viewHeroPage(e){
-  let path = `${window.location.pathname} + /../superhero.html#id=${e.target.parentElement.id}`;
-  window.open(path);
-}
-
-// add a hero to favourites
-async function addToFavourites(e){
-  let id = e.target.parentElement.id;
-  let favs = getFavs();
-  if(!favs.includes(id)){
-    favs.push(id);
-  }
-  localStorage.setItem('favHeros', JSON.stringify(favs));
-  e.target.innerHTML = 'Remove from favourites';
-  e.target.removeEventListener('click', addToFavourites);
-  e.target.addEventListener('click', removeFromFavourites);
-}
-
-// remove a hero from favourites
-async function removeFromFavourites(e){
-  let id = e.target.parentElement.id;
-  let favs = getFavs();
-
-  let updatedFavs = favs.filter(function(val){
-    return val != id;
-  })
-  localStorage.setItem('favHeros', JSON.stringify(updatedFavs));
-  e.target.innerHTML = 'Add to favourites';
-  e.target.removeEventListener('click', removeFromFavourites);
-  e.target.addEventListener('click', addToFavourites);
-}
-
-// retrieve a list of favourite hero id's from local storage
-function getFavs(){
-  let favs;
-  if(localStorage.getItem('favHeros') === null){
-    favs = [];
-  }
-  else{
-    favs = JSON.parse(localStorage.getItem('favHeros'));
-  }
-  return favs; 
+// This Function will display the Data on the Screen
+function display(data){
+    var superHeroList = document.getElementById('superhero-list');
+    superHeroList.innerHTML = "";
+    var results = data.data.results;
+//  Printing the results that are get from searched Query
+    console.log(results);
+    if(!results){
+        //  if Search character matches the results then only it will forward to next step
+        document.getElementById('search-character').value = "";
+        window.alert("No super hero found!");
+    }else{
+        //  Else the process it going on
+        // Creating a For Loop because there will be n number of results for same query
+        for(let result of results){
+            var templateCanvas = canvas.content.cloneNode(true);
+//  Get all the elemets from id and then changes its Inner HTMl
+            templateCanvas.getElementById("name").innerHTML = '<b>Name: </b> ' + result.name;
+            templateCanvas.getElementById("id").innerHTML = '<b>Hero ID: </b> ' + result.id ;
+            templateCanvas.getElementById("comic").innerHTML = '<b>Comic Available: </b>'+ result.comics.available ;
+            templateCanvas.getElementById("series").innerHTML = '<b>Series Available: </b>'+ result.series.available ;
+            templateCanvas.getElementById("stories").innerHTML = '<b>Stories Available: </b>'+ result.stories.available ;
+            //  Set Event listenet for Learn  more button 
+            templateCanvas.getElementById('learn-more').addEventListener('click', function(){
+                localStorage.setItem('id', result.id);
+                window.location.assign('./about.html');
+            });
+            //  Set Event listenet for Fav  more button 
+            templateCanvas.getElementById('fav').addEventListener('click', function(){
+                var index = localStorage.length;
+                var data = JSON.stringify(result);
+                localStorage.setItem(result.id,data);
+            });
+            superHeroList.appendChild(templateCanvas);
+        }
+    }
+};
+//  This is a function for displaying a alert box type message on the bottom of the screen, when we add to fav. heros.
+function addFunction() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
